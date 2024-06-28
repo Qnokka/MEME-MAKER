42a3750da301cf995317bdb20d1a2add2b6b82a4
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-06-28 20:37:30 +0900

    1.1 First Drawing

---
+++ app.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("canvas");
+//canvas element에 접근하기 위한 상수 선언
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+//canvas의 size를 알려주는 구문
+
+ctx.fillRect(50, 50, 100, 200);
+
+
+
+
