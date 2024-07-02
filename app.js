b0fdeea03552d0dc67077d4d0b6e2c5f72bf479a
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-07-02 14:20:19 +0900

    1.2 Paths

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ canvas.width = 800;
 canvas.height = 800;
 //canvas의 size를 알려주는 구문
 
-ctx.fillRect(50, 50, 100, 200);
+//ctx.fillRect(50, 50, 100, 200); //단축함수
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill(); 
 
-
-
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+setTimeout(() => {ctx.fill();}, 5000);
 
