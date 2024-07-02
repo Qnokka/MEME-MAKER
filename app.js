7574acedc833f80751278eaeb887632bf001a0a4
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-07-02 14:35:21 +0900

    1.3 moveTo and lineTo

--- app.js
+++ app.js
@@ -5,15 +5,10 @@ canvas.width = 800;
 canvas.height = 800;
 //canvas의 size를 알려주는 구문
 
-//ctx.fillRect(50, 50, 100, 200); //단축함수
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill(); 
-
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-setTimeout(() => {ctx.fill();}, 5000);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50)
+ctx.fill();
 
