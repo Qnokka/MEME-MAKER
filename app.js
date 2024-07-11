5f495495b3c9ef427869dcd3bb35c102b05bdc07
Qnokka <106862472+Qnokka@users.noreply.github.com>
2024-07-11 20:32:39 +0900

    2.2 Line Width

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 //canvas element에 접근하기 위한 상수 선언
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -13,16 +14,22 @@ function onMove(event) {
       ctx.stroke();
       return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
   }
-  function startPainting() {
-    isPainting = true;
-  }
-  function cancelPainting() {
-    isPainting = false;
-  }
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
